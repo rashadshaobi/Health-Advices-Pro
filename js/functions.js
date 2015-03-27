3e98b6b4903d3435b541fa52ecca37177e32cda5
Rashad Shaobi <rshaobi@gmail.com>
2015-03-27 09:03:04 -0700

    1st commit

---
+++ js/functions.js
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta content="">
+        
+        <title>Health Advice</title>
+        
+        
+    </head>
+    <body>
+        hello world
+    </body>
+</html>
